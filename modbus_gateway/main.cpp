97d3575ed1cbc7e38f1d84e94302934d2a311e59
Timofey <petukhovtd@yandex.ru>
2022-08-14 15:38:29 +0700

    wip modbus tcp master

--- modbus_gateway/main.cpp
+++ modbus_gateway/main.cpp
@@ -5,8 +5,27 @@
 #include <exchange/exchange.h>
 
 #include <queue>
+#include <iostream>
 
 using namespace modbus_gateway;
+
+class SingleRouter: public IRouter
+{
+public:
+     explicit SingleRouter( exchange::ActorId id )
+     : id_( id )
+     {}
+
+     ~SingleRouter() override = default;
+
+     [[nodiscard]] exchange::ActorId Route( modbus::UnitId id ) const override
+     {
+          return id;
+     }
+
+private:
+     exchange::ActorId id_;
+};
 
 class ModbusTcpMaster: public exchange::ActorHelper< ModbusTcpMaster >
 {
@@ -14,7 +33,8 @@ public:
      ModbusTcpMaster( const ContextPtr& context, const asio::ip::address& addr, asio::ip::port_type port, std::chrono::milliseconds timeout )
      : socket_( std::make_unique< TcpSocketUPtr::element_type >( *context ) )
      , ep_( addr, port )
-     , timer_( socket_->get_executor(), timeout )
+     , timeout_( timeout )
+     , timer_( socket_->get_executor() )
      , m_()
      , messageQueue_()
      , currentMessage_( nullptr )
@@ -26,7 +46,6 @@ public:
           asio::error_code ec;
           if( socket_->is_open() )
           {
-               socket_->cancel( ec );
                socket_->close( ec );
           }
      }
@@ -75,26 +94,28 @@ private:
 
      void StartConnectTaskUnsafe()
      {
-          FMT_LOG_TRACE( "ModbusTcpMaster::StartConnectTask" )
+          FMT_LOG_TRACE( "StartMessageTask::StartConnectTaskUnsafe: connect to {}:{}", ep_.address().to_string(), ep_.port() )
           Weak weak = GetWeak();
           socket_->async_connect( ep_, [ weak ]( asio::error_code ec )
           {
                Ptr self = weak.lock();
                if( !self )
                {
-                    FMT_LOG_CRIT( "ModbusTcpMaster: actor was deleted" )
+                    FMT_LOG_CRIT( "ModbusTcpMaster: connect: actor was deleted" )
                     return;
                }
 
-               if( ( asio::error::eof == ec ) || ( asio::error::connection_reset == ec ) )
+               if( ec )
                {
-                    FMT_LOG_INFO( "ModbusTcpMaster: disconnected: {}", ec.message() )
+                    FMT_LOG_ERROR( "ModbusTcpMaster: connect: error: {}", ec.message() )
+                    self->currentMessage_.reset();
+                    if( ( asio::error::operation_aborted == ec ) )
+                    {
+                         FMT_LOG_INFO( "ModbusTcpMaster: connect: canceled" )
+                         return;
+                    }
+                    FMT_LOG_INFO( "ModbusTcpMaster: connect: start new connect tast" )
                     self->StartConnectTask();
-                    return;
-               }
-               if( asio::error::operation_aborted == ec )
-               {
-                    FMT_LOG_INFO( "ModbusTcpMaster: canceled: {}", ec.message() )
                     return;
                }
                FMT_LOG_INFO( "ModbusTcpMaster: connect to {}:{}",
@@ -142,18 +163,26 @@ private:
                Ptr self = weak.lock();
                if( !self )
                {
-                    FMT_LOG_CRIT( "ModbusTcpMaster: actor was deleted" )
+                    FMT_LOG_CRIT( "ModbusTcpMaster: send: actor was deleted" )
                     return;
                }
 
-               if( ( asio::error::eof == ec ) || ( asio::error::connection_reset == ec ) ||
-                   ( asio::error::operation_aborted == ec ) )
+               std::lock_guard< std::mutex > lock( self->m_ );
+
+               if( ec )
                {
-                    FMT_LOG_INFO( "ModbusTcpMaster: disconnected: {}", ec.message() )
+                    FMT_LOG_INFO( "ModbusTcpMaster: send: error {}", ec.message() )
+                    self->currentMessage_.reset();
+                    if( ( asio::error::eof == ec ) || ( asio::error::connection_reset == ec ) )
+                    {
+                         FMT_LOG_INFO( "ModbusTcpMaster: send: start new connect task" )
+                         self->StartConnectTask();
+                    }
                     return;
                }
 
-
+               self->StartWaitTask();
+               self->StartReceiveTask();
           });
      }
 
@@ -161,15 +190,94 @@ private:
      {
           FMT_LOG_TRACE( "ModbusTcpMaster::StartWaitTask" )
           Weak weak = GetWeak();
+          timer_.expires_after( timeout_ );
           timer_.async_wait( [ weak ]( asio::error_code ec )
                              {
+                                  Ptr self = weak.lock();
+                                  if( !self )
+                                  {
+                                       FMT_LOG_CRIT( "ModbusTcpMaster: wait: actor was deleted" )
+                                       return;
+                                  }
 
+                                  std::lock_guard< std::mutex > lock( self->m_ );
+
+                                  if( ec )
+                                  {
+                                       if( asio::error::operation_aborted == ec )
+                                       {
+                                            FMT_LOG_TRACE( "ModbusTcpMaster: wait: canceled" )
+                                            return;
+                                       }
+                                       FMT_LOG_ERROR( "ModbusTcpMaster: wait: error: {}", ec.message() )
+                                       return;
+                                  }
+
+                                  FMT_LOG_TRACE( "ModbusTcpMaster: wait: achieve timeout, cancel receive task" )
+                                  self->currentMessage_.reset();
+                                  self->socket_->cancel( ec );
                              });
+     }
+
+     void StartReceiveTask()
+     {
+          FMT_LOG_TRACE( "ModbusTcpMaster::StartReceiveTask" )
+          auto modbusBuffer = std::make_shared< modbus::ModbusBuffer >( modbus::FrameType::TCP );
+          Weak weak = GetWeak();
+          socket_->async_receive( asio::buffer( modbusBuffer->begin().operator->(), modbusBuffer->GetAduSize() ),
+                                  [ weak, modbusBuffer ]( asio::error_code ec, size_t size )
+                                  {
+                                       Ptr self = weak.lock();
+                                       if( !self )
+                                       {
+                                            FMT_LOG_CRIT( "ModbusTcpMaster: receive: actor was deleted" )
+                                            return;
+                                       }
+
+                                       std::lock_guard< std::mutex > lock( self->m_ );
+
+                                       try
+                                       {
+                                            self->timer_.cancel();
+                                       }
+                                       catch( const asio::system_error& e )
+                                       {
+                                            FMT_LOG_ERROR( "ModbusTcpMaster: timer cancel error: {}", ec.message() )
+                                       }
+
+                                       if( ec )
+                                       {
+                                            if( asio::error::operation_aborted == ec )
+                                            {
+                                                 FMT_LOG_DEBUG( "ModbusTcpMaster: receive: canceled" )
+                                                 return;
+                                            }
+                                            FMT_LOG_ERROR( "ModbusTcpMaster: receive: error: {}", ec.message() )
+                                            self->currentMessage_.reset();
+                                            return;
+                                       }
+
+                                       FMT_LOG_TRACE( "ModbusTcpMaster: receive: {} bytes", size )
+                                       modbusBuffer->SetAduSize( size );
+
+                                       const auto currentMessage = self->currentMessage_;
+                                       self->currentMessage_.reset();
+
+                                       const ModbusMessageInfo currentInfo = currentMessage->GetModbusMessageInfo();
+
+                                       {
+                                             // Check message here
+                                       }
+
+                                       exchange::Exchange::Send( currentInfo.GetSourceId(),
+                                                                 ModbusMessage::Create( currentInfo, modbusBuffer ) );
+                                  });
      }
 
 private:
      TcpSocketUPtr socket_;
      asio::ip::tcp::endpoint ep_;
+     TimeoutMs timeout_;
      asio::basic_waitable_timer< std::chrono::steady_clock > timer_;
      std::mutex m_;
      std::queue< ModbusMessagePtr > messageQueue_;
@@ -178,13 +286,35 @@ private:
 
 int main()
 {
-//     TimeoutMs timeoutMs = 1000;
-//     RouterPtr router = std::make_shared< Router >();
-//     FmtLogger::SetLogLevel( FmtLogger::LogLevel::Trace );
-//     ContextPtr context = std::make_shared< ContextPtr::element_type >();
-//     TcpServer::Ptr tcpServer = TcpServer::Create( context, 1234, timeoutMs, router );
-//     exchange::Exchange::Insert( tcpServer );
-//     tcpServer->Start();
-//     context->run();
+     using namespace std::chrono_literals;
+
+     FmtLogger::SetLogLevel( FmtLogger::LogLevel::Trace );
+
+     ContextPtr context = std::make_shared< ContextPtr::element_type >();
+     auto work = asio::executor_work_guard( context->get_executor() );
+     auto contextThread = std::thread( [ context ]()
+                                       {
+                                            context->run();
+                                       } );
+
+     ModbusTcpMaster::Ptr tcpMaster = ModbusTcpMaster::Create( context, asio::ip::make_address( "10.10.20.22"), 502, 1000ms );
+     const auto tcpMasterId = exchange::Exchange::Insert( tcpMaster );
+
+     RouterPtr router = std::make_shared< SingleRouter >( tcpMasterId );
+
+     ModbusTcpServer::Ptr tcpServer = ModbusTcpServer::Create( context, asio::ip::address_v4::any(), 502, router );
+     exchange::Exchange::Insert( tcpServer );
+     tcpServer->Start();
+
+     std::cin.get();
+
+     tcpServer->Stop();
+
+     context->stop();
+     if( contextThread.joinable() )
+     {
+          contextThread.join();
+     }
+
      return 0;
 }
