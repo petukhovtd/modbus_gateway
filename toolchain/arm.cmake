8b5a32c3b176e345ef2e34b0602a88d87ef8f386
Timofey <petukhovtd@yandex.ru>
2024-01-08 21:54:21 +0700

    Add cross compile in Docker

---
+++ toolchain/arm.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(prefix arm-linux-gnueabi)
+
+set(CMAKE_C_COMPILER /usr/bin/${prefix}-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/${prefix}-g++)
+set(CMAKE_AR /usr/bin/${prefix}-ar)
